e911a68c6454b93641f1f93f4ae93c50fe96d3f2
Sergey Scherbina <sergey.scherbina@gmail.com>
2013-05-04 02:34:31 +0300

    leaf iterator

--- org.tmatesoft.sqljet2/src/main/java/org/tmatesoft/sqljet2/internal/btree2/BTreeIterator.java
+++ org.tmatesoft.sqljet2/src/main/java/org/tmatesoft/sqljet2/internal/btree2/BTreeIterator.java
@@ -8,28 +8,23 @@ import org.tmatesoft.sqljet2.internal.pager.Page;
 import org.tmatesoft.sqljet2.internal.system.Pointer;
 import org.tmatesoft.sqljet2.internal.system.Trouble;
 
-public abstract class BTreeIterator implements Iterator<Pointer> {
+public class BTreeIterator {
 
-	protected final Page page;
+	private static class CellsIterator implements Iterator<Pointer> {
 
-	protected BTreeIterator(final Page page) {
-		this.page = page;
-	}
-
-	protected int getCellsCount() {
-		return PageHeader.getCellsCount(page);
-	}
-	
-	protected Pointer getCell(final int index) {
-		return PageHeader.getCell(page,index);
-	}
-
-	public static class LeafIterator extends BTreeIterator {
-
+		protected final Page page;
 		private int index = 0;
 
-		public LeafIterator(Page root) {
-			super(root);
+		public CellsIterator(final Page page) {
+			this.page = page;
+		}
+
+		protected int getCellsCount() {
+			return PageHeader.getCellsCount(page);
+		}
+
+		protected Pointer getCell(final int index) {
+			return PageHeader.getCell(page, index);
 		}
 
 		public boolean hasNext() {
@@ -37,10 +32,38 @@ public abstract class BTreeIterator implements Iterator<Pointer> {
 		}
 
 		public Pointer next() {
-			if(!hasNext()) {
+			if (hasNext()) {
+				return getCell(index++);
+			} else {
 				throw new NoSuchElementException();
 			}
-			return getCell(index++);
+		}
+
+		public void remove() {
+		}
+	}
+
+	public static class LeafIterator implements Iterator<BTreeRecord> {
+
+		private final CellsIterator cells;
+
+		public LeafIterator(final Page page) {
+			cells = new CellsIterator(page);
+		}
+
+		public boolean hasNext() {
+			return cells.hasNext();
+		}
+
+		public BTreeRecord next() {
+			try {
+				final Pointer cell = cells.next();
+				return new BTreeRecord(cell.getMemory().getPointer(
+						TableLeafCell.getPayloadOffset(cell.getMemory(),
+								cell.getAddress())));
+			} catch (Trouble e) {
+				throw new RuntimeException(e);
+			}
 		}
 
 		public void remove() {
--- org.tmatesoft.sqljet2/src/test/java/org/tmatesoft/sqljet2/test/TestDb2.java
+++ org.tmatesoft.sqljet2/src/test/java/org/tmatesoft/sqljet2/test/TestDb2.java
@@ -2,28 +2,24 @@ package org.tmatesoft.sqljet2.test;
 
 import org.junit.Test;
 import org.tmatesoft.sqljet2.internal.btree.BTreeRecord;
-import org.tmatesoft.sqljet2.internal.btree2.BTreeIterator;
-import org.tmatesoft.sqljet2.internal.btree2.TableLeafCell;
+import org.tmatesoft.sqljet2.internal.btree2.BTreeIterator.LeafIterator;
 import org.tmatesoft.sqljet2.internal.pager.Pager;
 import org.tmatesoft.sqljet2.internal.pager.impl.FilePager;
 import org.tmatesoft.sqljet2.internal.system.FileSystem.OpenPermission;
-import org.tmatesoft.sqljet2.internal.system.Pointer;
 import org.tmatesoft.sqljet2.internal.system.Trouble;
 
 public class TestDb2 {
-	
+
 	private final static String TEST_DB = "src/test/resources/db/testdb.sqlite";
 
 	@Test
-	public void testForward() throws Trouble {
+	public void testIterator() throws Trouble {
 		final Pager pager = new FilePager();
 		pager.open(TEST_DB, OpenPermission.READONLY);
 		try {
-			final BTreeIterator t = new BTreeIterator.LeafIterator(pager.readPage(1));
-			while(t.hasNext()){
-				final Pointer p = t.next();
-				int payloadOffset = TableLeafCell.getPayloadOffset(p.getMemory(), p.getAddress());
-				final BTreeRecord r = new BTreeRecord(p.getMemory().getPointer(payloadOffset));
+			final LeafIterator t = new LeafIterator(pager.readPage(1));
+			while (t.hasNext()) {
+				final BTreeRecord r = t.next();
 				for (int i = 0; i < r.getColumnsCount(); i++) {
 					System.out.println(r.getValue(i));
 				}
